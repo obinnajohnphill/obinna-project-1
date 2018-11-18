95f58e251070f5dfae80067c167795708493409f
Obinna Johnphill <obinna.johnphill@googlemail.com>
2018-11-18 00:20:11 +0000

    updated README.md file

--- parser.php
+++ parser.php
@@ -17,28 +17,30 @@ $dump = new ProcessData($_GET);
 
 class DumpData{
 
-    public function __construct( array $data)
+    public function __construct(array $data)
     {
         $this->dumpData($data);
     }
 
     function dumpData($data){
-    ## Dump the processed content of the csv file onto the terminal
-    foreach ($data as $fields){
-        if(empty($fields['brand_name']) or empty($fields['model_name'])){
-            throw new Exception("Required field is not set");
-        }
-        echo "\n";
-        echo "make: ".$fields['brand_name']."(string, required)"."- Brand name"."\n";
-        echo "model: ".$fields['model_name']."(string, required)"."- Model name"."\n";
-        echo "colour: ".$fields['colour_name']."(string)"."- Colour name"."\n";
-        echo "capacity: ".$fields['gb_spec_name']."(string)"."- GB Spec name"."\n";
-        echo "network: ".$fields['network_name']."(string)"."- Network name"."\n";
-        echo "grade: ".$fields['grade_name']."(string)"." - Grade Name"."\n";
-        echo "condition: ".$fields['condition_name']."(string)"."- Condition name"."\n";
-        echo "\n";
-    };
-
+        ## Dump the processed content of the csv file onto the terminal
+        foreach ($data as $fields){
+            if(empty($fields['brand_name']) or empty($fields['model_name'])){
+                throw new Exception("Required field is not set");
+            }
+            echo "\n";
+            echo "make: ".$fields['brand_name']." (string,required)"."- Brand name"."\n";
+            echo "model: ".$fields['model_name']." (string,required)"."- Model name"."\n";
+            echo "colour: ".$fields['colour_name']." (string)"."- Colour name"."\n";
+            echo "capacity: ".$fields['gb_spec_name']." (string)"."- GB Spec name"."\n";
+            echo "network: ".$fields['network_name']." (string)"."- Network name"."\n";
+            echo "grade: ".$fields['grade_name']." (string)"." - Grade Name"."\n";
+            echo "condition: ".$fields['condition_name']." (string)"."- Condition name"."\n";
+            echo "\n";
+        };
+        echo "--------------------------------------\n";
+        echo "Enter: Ctrl+C (to complete the process)\n";
     }
+
 }
 
